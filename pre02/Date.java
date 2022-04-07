950de4788648b84eabca69db09c178490083d0f7
GEORGIA GROSSETT-DALE <97038617+GGDale@users.noreply.github.com>
2022-04-07 02:30:23 +0000

    2.12 Data.java

---
+++ pre02/Date.java
@@ -0,0 +1,11 @@
+
+String day = Thursday;
+Int date = 16;
+String month= July;
+Int year = 2022;
+System.out.print("The current date");
+System.out.print("Thursday");
+System.out.print("month");
+System.out.print("date");
+System.out.print("year'");
+System.out.println(".");
