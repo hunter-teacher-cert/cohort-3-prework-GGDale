8ddbfc999f456789b3bd076477736ca17b413ad7
GEORGIA GROSSETT-DALE <97038617+GGDale@users.noreply.github.com>
2022-04-12 21:22:31 +0000

    Date.java

---
+++ Date.java
@@ -0,0 +1,13 @@
+public class Date {
+    public static void main(String[] args){
+String day = "Thursday";
+int date = 16;
+String month= "July";
+int year= 2022;
+System.out.println(day);
+System.out.println(date);
+System.out.println(month);
+System.out.println(year);
+System.out.println(".");
+    }
+}
