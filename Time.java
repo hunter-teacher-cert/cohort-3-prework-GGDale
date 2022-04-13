62cda93d1259954125e885e26a265ac316c17d44
GEORGIA GROSSETT-DALE <97038617+GGDale@users.noreply.github.com>
2022-04-13 02:20:16 +0000

    Ex 3-#3 Calculate # sec to midnight

---
+++ Time.java
@@ -0,0 +1,9 @@
+public class Time {
+public static void main(String[] args){
+int hour = 10;
+int minute = 59;
+int second = 59;
+System.out.println("Number of seconds since midnight:" +(second + (minute*60) + (hour*3600)));
+System.out.println("Number of seconds to midnight:" +((60-second) + ((60-1-minute)*60) + (24-1-hour)*3600));
+      }
+}
